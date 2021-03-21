e29c5dade5651dce1d3bb7c4703bbf1584fa3c26
[karolina1434] <[karolina.napieracz@gmail.com]>
2021-03-21 11:00:32 +0100

    new project

--- scratch_15.py
+++ scratch_15.py
@@ -1,7 +1,15 @@
 print('KArolina')
 a = 14
 b = 16
+x = int(input("podaj liczbe godzin"))
+y = int(input("podaj liczbe zajzdów"))
 def liczenie(a,b):
     wynik = a + b
     print (wynik)
 print (liczenie(a,b))
+
+def zajęcia(x,y):
+    ilgodz= x*y
+    śr = ilgodz/y
+    print(ilgodz,śr)
+print(zajęcia(x,y))
